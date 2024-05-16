2fb742ce5123974bccfd45d4ce93226a1fefddfb
Jacopo Guelpa <jacopo.guelpa@gmail.com>
2024-05-16 12:25:03 +0200

    first commit

---
+++ src/context/Context.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+import runChat from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [previewsPrompts, setPreviewsPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const onSent = async (prompt) => {
+    await runChat(prompt);
+  };
+
+  const contextValue = {};
+
+  return <Context.Provider value={contextValue}>{props.children}</Context.Provider>;
+};
+
+export default ContextProvider;
